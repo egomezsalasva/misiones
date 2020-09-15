08a2431ae13d3b2d765f347938e165ad54216f00
Kike <kike@Kikes-MacBook-Pro.local>
2020-09-15 23:33:21 +0200

    Favicon Backgorund

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
+
+      <div className="background"/>
+      <div className="background02"/>
+
+      <div className="audioWrapper">
+        <audio  src="./audio/Missiones01.mp3" controls autoPlay/>
+      </div>
+
+      
       
     </div>
   );
